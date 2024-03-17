fdd783f79158026c11905b16983efe5d73030f48
Raunit Raj <128997347+raunit1610@users.noreply.github.com>
2024-03-17 12:42:23 +0530

    Add files via upload

---
+++ Boot_prac.js
@@ -0,0 +1,47 @@
+// JavaScript to handle theme change
+const themeDropdown = document.getElementById('bd-theme-dropdown');
+const themeButtons = document.querySelectorAll('[data-bs-theme-value]');
+
+themeButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const themeValue = this.getAttribute('data-bs-theme-value');
+    document.documentElement.setAttribute('data-bs-theme', themeValue);
+    localStorage.setItem('theme', themeValue);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    document.documentElement.setAttribute('data-bs-theme', savedTheme);
+  }
+});
+
+//Like Button Blinks
+const likeButtons = document.querySelectorAll('.like-button');
+
+likeButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const icon = this.querySelector('svg');
+    const message = document.createElement('div');
+    message.classList.add('thank-you-message');
+    message.textContent = 'Thank you for your like!';
+    message.style.fontFamily = 'cursive';
+    
+    // Add blink class for blinking effect
+    icon.classList.add('blink');
+    
+    // Append thank you message to the parent element of the button
+    button.parentNode.appendChild(message);
+    
+    // Remove blink class and thank you message immediately
+    setTimeout(() => {
+      icon.classList.remove('blink');
+      button.parentNode.removeChild(message);
+    }, 1000);
+  });
+});
+
+
+
+
