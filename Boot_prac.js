4130138388e7204d50f53e4e7c2c9357867a7aba
Raunit Raj <128997347+raunit1610@users.noreply.github.com>
2024-07-20 22:35:09 +0530

    Update Boot_prac.js

--- Boot_prac.js
+++ Boot_prac.js
@@ -53,3 +53,32 @@ viewButtons.forEach(button => {
     }
   });
 });
+
+const containerEl = document.querySelector(".conte");
+
+const careers = ["Web Developer", "Freelancer", "UI UX Designer", "Data Analyst"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+    <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
+    careerIndex
+  ].slice(0, characterIndex)}</h1>
+    `;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
+}
